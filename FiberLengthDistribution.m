415ad211f487d313eea3e4f2ef2c916653737fd3
Joe Carini <47539123+jacarini@users.noreply.github.com>
2019-08-05 09:51:50 -0400

    Add files via upload

--- FiberLengthDistribution.m
+++ FiberLengthDistribution.m
@@ -16,23 +16,23 @@ for i = 1:size(Folder,1)
     d = imread(Folder(i).name);
     
     BW = im2bw(d);
-    BW = ~BW; %figure; imshow(BW); %figure1
+    BW = ~BW; %figure; imshow(BW);                                  %figure1
     [~,threshold] = edge(BW,'sobel');
     fudgeFactor = 0.3;
     BW = edge(BW,'sobel',threshold * fudgeFactor); 
     se90 = strel('line',2,90);
     se0 = strel('line',2,0);
-    BW = imdilate(BW,[se90 se0]); %figure; imshow(BW); %figure2
+    BW = imdilate(BW,[se90 se0]); %figure; imshow(BW);              %figure2
     se1 = strel('diamond',3);
     BW = ~BW; BW = imerode(BW,se1);
-    BW =  bwareaopen(BW,10,4); %figure; imshow(BW); %figure3  
+    BW =  bwareaopen(BW,10,4); %figure; imshow(BW);                 %figure3  
     se0 = strel('line',4,0);
     BW = imdilate(BW,[se90 se0]);
     BW = imfill(BW,'holes');
     BW = imerode(BW,se1);
-    BW = imfill(BW,'holes'); %figure; imshow(BW); %figure 4
-    BW =  bwareaopen(BW,5000,8);% figure; imshow(BW); %figure 5
-    BW = imclearborder(BW,4); %figure; imshow(BW); %figure 6
+    BW = imfill(BW,'holes'); %figure; imshow(BW);                   %figure 4
+    BW =  bwareaopen(BW,5000,8);% figure; imshow(BW);               %figure 5
+    BW = imclearborder(BW,4); %figure; imshow(BW);                  %figure 6
     
     CC = bwconncomp(BW);
     properties = regionprops(CC,'MajorAxisLength', 'MinorAxisLength');
